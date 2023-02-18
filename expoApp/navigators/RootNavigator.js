ed82f8d24cde0488c48ea1e20cea90c586581daa
teleportsing <teleportsing@gmail.com>
2023-02-18 18:13:43 +0800

    modify SignUp

--- expoApp/navigators/RootNavigator.js
+++ expoApp/navigators/RootNavigator.js
@@ -2,14 +2,16 @@ import * as React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import WelcomeScreen from "../screens/WelcomeScreen";
 import SubscribeScreen from "../screens/SubscribeScreen";
+import SignUp from "../screens/SignUp";
 
 const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen name="Upwork" component={WelcomeScreen} />
       <Stack.Screen name="Subscribe" component={SubscribeScreen} />
+      <Stack.Screen name="SignUp" component={SignUp} />
     </Stack.Navigator>
   );
 };
--- expoApp/screens/WelcomeScreen.js
+++ expoApp/screens/WelcomeScreen.js
@@ -5,22 +5,30 @@ import Button from "../components/Button";
 const WelcomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <View style={styles.contentContainer}>
-        <Image
-          style={styles.logo}
-          source={require("../assets/little-lemon-logo.png")}
-        />
+      <View style={styles.indexText}>
+      <Image
+            style={styles.logo}
+            source={require("../assets/upwork/upwork-logo.png")}
+          />
+      </View>
+      
+      <View style={styles.indexText}>
         <Text style={styles.title}>
-          Little Lemon, your local Mediterranean Bistro
+        Never miss an opportunity.
+        </Text>
+        <Text style={styles.describe}>
+        Easily find work , chat ,  and collaborate on the go.
         </Text>
       </View>
-      <Button
-        onPress={() => {
-          navigation.navigate("Subscribe");
-        }}
-      >
-        Newsletter
-      </Button>
+      <View style={styles.indexButton}>
+        <Button  
+          onPress={() => {
+              navigation.navigate("SignUp");
+            }}
+          >
+            Log In
+          </Button>
+      </View>
     </View>
   );
 };
@@ -28,27 +36,66 @@ const WelcomeScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
+    paddingBottom: 154,
     backgroundColor: 'white',
     justifyContent: 'space-between',
   },
-  contentContainer: {
+  indexLog: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
+    padding: 0,
+    margin: 0
   },
   logo: {
-    height: 200,
-    width: 300,
+    height: 150,
+    width: 200,
     resizeMode: "contain",
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  },
+  indexSearch: {
+    height: 150,
+    width: '100%',
+   
+    resizeMode: "contain",
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  },
+  indexText: {
+    paddingLeft: 24,
+    paddingRight: 24,
+    flex: 1,
+    alignItems: 'center',
   },
   title: {
-    marginTop: 48,
-    paddingVertical: 10,
+    width: '60%',
+    flex: 0.8,
     color: "#333333",
     textAlign: "center",
-    fontSize: 20,
+    fontSize: 22,
     fontWeight: "bold",
+    flexWrap: 'wrap'
+  },
+  describe: {
+    width: '76%',
+    flex: 0.8,
+    paddingVertical: 1,
+    color: "#111",
+    textAlign: "center",
+    fontSize: 16
+  },
+  indexButton: {
+    width: '76%',
+    flex: 0.8,
+    paddingVertical: 1,
+    color: "#111",
+    textAlign: "center",
+    fontSize: 16,
+    marginLeft: 'auto',
+    marginRight: 'auto'
   },
 });
 
